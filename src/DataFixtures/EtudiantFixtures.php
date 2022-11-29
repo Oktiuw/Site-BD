7e912aa6cd7755bb0cd408f8b92067b2948f49e3
Aurelien VINCENT <aurelien.vincent@etudiant.univ-reims.fr>
2022-11-29 16:43:35 +0100

    EtudiantFixtures OK

--- src/DataFixtures/EtudiantFixtures.php
+++ src/DataFixtures/EtudiantFixtures.php
@@ -3,6 +3,8 @@
 namespace App\DataFixtures;
 
 use App\Entity\Utilisateur;
+use App\Factory\EtudiantFactory;
+use App\Factory\UtilisateurFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
@@ -11,6 +13,11 @@ class EtudiantFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
+        EtudiantFactory::createMany(50,function ()
+        {
+            $lastname=EtudiantFactory::faker()->unique()->lastName();
+            return ['nomEtud'=>"$lastname",'cdUtil'=>UtilisateurFactory::createOne(["roles"=>["ROLE_ETUDIANT,ROLE_USER"],"login"=>strtolower(str_replace(" ","","$lastname".rand(1,300)))])];
+        });
     }
 
     public function getDependencies()
