8d97be20fc2fb554762290569a2c5e7095b27c03
Aurélien Vincent <aurelien.vincent@etudiants.univ-reims.fr>
2022-11-30 11:17:51 +0100

    EnseignantFixture Test

--- src/DataFixtures/EnseignantFixtures.php
+++ src/DataFixtures/EnseignantFixtures.php
@@ -2,6 +2,8 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\EnseignantFactory;
+use App\Factory\UtilisateurFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,9 +11,12 @@ class EnseignantFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
-
-        $manager->flush();
+        EnseignantFactory::createMany(5,function ()
+        {
+            $lastname=EnseignantFactory::faker()->unique()->lastName();
+            return ['nomEtud'=>"$lastname",'cdUtil'=>UtilisateurFactory::createOne(["roles"=>["ROLE_ENSEIGNANT"],"login"=>strtolower(str_replace(" ","","$lastname".rand(1,300)))])];
+        });
+        EnseignantFactory::createOne((array)UtilisateurFactory::createOne(["roles" => ["ROLE_ENSEIGNANT"], "login" => "Enseignant"]));
+        EnseignantFactory::createOne((array)UtilisateurFactory::createOne(["roles" => ["ROLE_ADMIN,ROLE_ENSEIGNANT"], "login" => "EnseignantAdmin"]));
     }
 }
