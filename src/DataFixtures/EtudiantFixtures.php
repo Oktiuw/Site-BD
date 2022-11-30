1ca89c7ae178d9f97772eb7949059870bc31b96f
Aurélien Vincent <aurelien.vincent@etudiants.univ-reims.fr>
2022-11-30 11:03:40 +0100

    Fin MAJEtudiantFixture

--- src/DataFixtures/EtudiantFixtures.php
+++ src/DataFixtures/EtudiantFixtures.php
@@ -16,8 +16,9 @@ class EtudiantFixtures extends Fixture implements DependentFixtureInterface
         EtudiantFactory::createMany(50,function ()
         {
             $lastname=EtudiantFactory::faker()->unique()->lastName();
-            return ['nomEtud'=>"$lastname",'cdUtil'=>UtilisateurFactory::createOne(["roles"=>["ROLE_ETUDIANT,ROLE_USER"],"login"=>strtolower(str_replace(" ","","$lastname".rand(1,300)))])];
+            return ['nomEtud'=>"$lastname",'cdUtil'=>UtilisateurFactory::createOne(["roles"=>["ROLE_ETUDIANT"],"login"=>strtolower(str_replace(" ","","$lastname".rand(1,300)))])];
         });
+        EtudiantFactory::createOne(['cdUtil'=>UtilisateurFactory::createOne(["login"=>"Etudiant","roles"=>["ROLE_ETUDIANT"]])]);
     }
 
     public function getDependencies()
