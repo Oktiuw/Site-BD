d1ed43804163f22e9787931b64d536f9984f2204
Aurelien VINCENT <aurelien.vincent@etudiant.univ-reims.fr>
2022-12-02 11:14:36 +0100

    Ajout user et findOne dans entreprise controller

--- src/Controller/EntrepriseController.php
+++ src/Controller/EntrepriseController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\EntrepriseRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class EntrepriseController extends AbstractController
 {
     #[Route('/entreprise', name: 'app_entreprise')]
-    public function index(): Response
+    public function index(EntrepriseRepository $entrepriseRepository): Response
     {
+        $user=$this->getUser();
+        $profile=$entrepriseRepository->findOneBy(['cdUtil'=>$user->getUserIdentifier()]);
         return $this->render('entreprise/index.html.twig', [
-            'controller_name' => 'EntrepriseController',
+            'user' =>$user,'profile'=>$profile
         ]);
     }
 }
