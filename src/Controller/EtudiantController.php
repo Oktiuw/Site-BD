c8f1309fbeb324f3ed3f2e6331d3a0dc65e11eda
sezg0002 <harun.sezgin@etudiant.univ-reims.fr>
2022-12-05 18:27:38 +0100

    avancement controller etudiant

--- src/Controller/EtudiantController.php
+++ src/Controller/EtudiantController.php
@@ -15,7 +15,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-
+#[IsGranted('ROLE_ETUDIANT')]
 class EtudiantController extends AbstractController
 
 {
