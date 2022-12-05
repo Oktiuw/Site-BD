9e51b3548c1601a4cd53a377df2b10a2d9807602
Aurelien VINCENT <aurelien.vincent@etudiant.univ-reims.fr>
2022-12-05 22:07:01 +0100

    Restriction

--- src/Controller/UtilisateurController.php
+++ src/Controller/UtilisateurController.php
@@ -12,7 +12,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\String\Slugger\SluggerInterface;
-
+#[IsGranted('IS_AUTHENTICATED_FULLY')]
 class UtilisateurController extends AbstractController
 {
     #[Route('/updateAvatar', name: 'app_utilisateur')]
