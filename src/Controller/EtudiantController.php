858d0730877749850a5b4e84e8548d43aa9a8b53
sezg0002 <harun.sezgin@etudiant.univ-reims.fr>
2022-12-05 15:55:58 +0100

    Ajout controller etudiant et twig

--- src/Controller/EtudiantController.php
+++ src/Controller/EtudiantController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\EntrepriseRepository;
+use App\Repository\EtudiantRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +11,16 @@ use Symfony\Component\Routing\Annotation\Route;
 class EtudiantController extends AbstractController
 {
     #[Route('/etudiant', name: 'app_etudiant')]
-    public function index(): Response
+    public function index(EtudiantRepository $etudiantRepository): Response
     {
+        $user=$this->getUser();
+        $profile=$etudiantRepository->findOneBy(['cdUtil'=>$user->getId()]);
+        $avatar=null;
+        if ($user->getAvatar() !== null) {
+            $avatar=$user->setAvatar(base64_encode(stream_get_contents($user->getAvatar())));
+        }
         return $this->render('etudiant/index.html.twig', [
-            'Etudiant' => 'EtudiantController',
+            'user' =>$user,'profile'=>$profile,'avatar'=>$avatar
         ]);
     }
 }
--- src/DataFixtures/EtudiantFixtures.php
+++ src/DataFixtures/EtudiantFixtures.php
@@ -13,10 +13,9 @@ class EtudiantFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
-        EtudiantFactory::createMany(50,function ()
-        {
+        EtudiantFactory::createMany(50, function () {
             $lastname=EtudiantFactory::faker()->unique()->lastName();
-            return ['nomEtud'=>"$lastname",'cdUtil'=>UtilisateurFactory::createOne(["roles"=>["ROLE_ETUDIANT"],"login"=>strtolower(str_replace(" ","","$lastname".rand(1,300)))])];
+            return ['nomEtud'=>"$lastname",'cdUtil'=>UtilisateurFactory::createOne(["roles"=>["ROLE_ETUDIANT"],"login"=>strtolower(str_replace(" ", "", "$lastname".rand(1, 300)))])];
         });
         EtudiantFactory::createOne(['cdUtil'=>UtilisateurFactory::createOne(["login"=>"Etudiant","roles"=>["ROLE_ETUDIANT"]])]);
     }
