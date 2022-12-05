1090d876097bfb3ef7eeaec9ef54afc7ab6bbd6a
sezg0002 <harun.sezgin@etudiant.univ-reims.fr>
2022-12-05 18:09:13 +0100

    modif update et twig non fini

--- src/Controller/EtudiantController.php
+++ src/Controller/EtudiantController.php
@@ -4,12 +4,15 @@ namespace App\Controller;
 
 use App\Repository\EntrepriseRepository;
 use App\Repository\EtudiantRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[IsGranted('ROLE_ETUDIANT')]
+
 class EtudiantController extends AbstractController
 
 {
@@ -26,4 +29,43 @@ class EtudiantController extends AbstractController
             'user' =>$user,'profile'=>$profile,'avatar'=>$avatar
         ]);
     }
+
+    #[Route('/etudiant/update')]
+    public function update(EtudiantRepository $etudiantRepository, ManagerRegistry $doctrine, Request $request): Response
+    {
+        $userType=new UtilisateurType();
+        $etudiantType=new EtudiantType();
+        $user=$this->getUser();
+        $avatar=null;
+        if ($user->getAvatar() !== null) {
+            $avatar=$user->setAvatar(base64_encode(stream_get_contents($user->getAvatar())));
+        }
+        $formUser=$this->createForm(UtilisateurType::class, $user)->add(
+            'submit',
+            SubmitType::class,
+            ['label' => 'Modifier']
+        );
+        $etudiant=$etudiantRepository->findOneBy(['cdUtil'=>$user->getId()]);
+        $form=$this->createForm(EtudiantType::class, $etudiant);
+        $form->handleRequest($request);
+        $formUser->handleRequest($request);
+        if ($formUser->isSubmitted() && $formUser->isValid()) {
+            $etudiantType=$form->getData();
+            $userType=$formUser->getData();
+            $entityManager=$doctrine->getManager();
+            $etudiant->setNomEnt($etudiantType->getNomEnt());
+            $etudiant->setNomRef($etudiantType->getNomRef());
+            $user->setLogin($userType->getLogin());
+            $user->setEmail($userType->getEmail());
+            $entityManager->flush();
+            return $this->redirectToRoute('app_etudiant');
+        }
+        return $this->renderForm('etudiant/update.html.twig', ['form'=>$form,'profile'=>$etudiant,'formUser'=>$formUser,'avatar'=>$avatar]);
+    }
+
 }
+
+
+
+
+
