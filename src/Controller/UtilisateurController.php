25abf71a3b440bc0a86fe55e230ad6c29e491e52
Aurelien VINCENT <aurelien.vincent@etudiant.univ-reims.fr>
2022-12-05 21:45:52 +0100

    creation nom fichier

--- src/Controller/UtilisateurController.php
+++ src/Controller/UtilisateurController.php
@@ -2,17 +2,30 @@
 
 namespace App\Controller;
 
+use App\Form\UtilisateurType;
+use GuzzleHttp\Psr7\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class UtilisateurController extends AbstractController
 {
-    #[Route('/utilisateur', name: 'app_utilisateur')]
-    public function index(): Response
+    #[Route('/updateAvatar', name: 'app_utilisateur')]
+    public function index(Request $request, SluggerInterface $slugger): Response
     {
-        return $this->render('utilisateur/index.html.twig', [
-            'controller_name' => 'UtilisateurController',
-        ]);
+        $user=$this->getUser();
+        $form=$this->createForm(UtilisateurType::class, $user);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $avatar=$form->get('photo')->getData();
+            if ($avatar) {
+                $originalFilename=pathinfo($avatar->getClientOriginalName(), PATHINFO_FILENAME);
+                $safeFilename=$slugger->slug($originalFilename);
+                $newFilename=$safeFilename.'-'.uniqid().'.'.$avatar->guessExtension();
+            }
+        }
+        return $this->renderForm('utilisateur/index.html.twig', [
+            'form' => $form ]);
     }
 }
