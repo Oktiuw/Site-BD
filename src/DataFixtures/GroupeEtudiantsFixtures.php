0cbe0ec8433b22c5ac29514145577878f6bae099
Fouad Belhia <fouad.belhia@etudiant.univ-reims.fr>
2022-12-07 16:05:41 +0100

    Création factory et fixtures GroupeEtudiants en cours

---
+++ src/DataFixtures/GroupeEtudiantsFixtures.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class GroupeEtudiantsFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+
+    }
+}
