ca99f5f805cf78a8a8cb2afb06812a94232ebf86
Aurelien VINCENT <aurelien.vincent@etudiant.univ-reims.fr>
2022-12-07 20:56:07 +0100

    Debut creation evenement

--- src/Controller/Admin/EvenementCrudController.php
+++ src/Controller/Admin/EvenementCrudController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Enseignant;
 use App\Entity\Evenement;
+use Doctrine\ORM\EntityRepository;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
@@ -22,7 +25,20 @@ class EvenementCrudController extends AbstractCrudController
         return [
             TimeField::new('hDeb')->renderAsChoice()->setLabel("Horaire de début"),
             TimeField::new('hFin')->setLabel("Horaire de fin")->renderAsChoice(),
-            DateField::new('dateEvmt')->setLabel("Date de l'evenement")
+            DateField::new('dateEvmt')->setLabel("Date de l'evenement"),
+            AssociationField::new('TypeEvenement')->setFormTypeOptions(['choice_label'=>'intTpEvmt','query_builder'=>function (EntityRepository $entityRepository) {
+                return $entityRepository->createQueryBuilder('c')->orderBy('c.intTpEvmt', 'ASC');
+            }])->formatValue(function ($value, $entity) {
+                return $entity->getTypeEvenement()->getIntTpEvmt();
+            }),
+            AssociationField::new('Enseignant')->setFormTypeOptions(['choice_label'=>function (Enseignant $enseignant) {
+                return strtoupper($enseignant->getNomEn()).' '.$enseignant->getPnomEn();
+            },'query_builder'=>function (EntityRepository $entityRepository) {
+                return $entityRepository->createQueryBuilder('c')->orderBy('c.nomEn', 'ASC');
+            }])->formatValue(function ($value, $entity) {
+                return $entity->getEnseignant()->getNomEn();
+            }),
+
         ];
     }
 }
