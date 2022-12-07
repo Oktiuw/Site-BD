2b62e474e254357262423f21973c9573a61b54d6
sezg0002 <harun.sezgin@etudiant.univ-reims.fr>
2022-12-07 16:31:39 +0100

    Modification formulaire pour etudiant

--- src/Form/EtudiantType.php
+++ src/Form/EtudiantType.php
@@ -4,18 +4,20 @@ namespace App\Form;
 
 use App\Entity\Etudiant;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
 
 class EtudiantType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('cvEtud')
-            ->add('nomEtud')
+            ->add('CV', FileType::class, ['label'=>'CV Etudiant ( pdf uniquement)','mapped'=>false,'required'=>false,
+                'constraints'=>[new File(['maxSize'=>'3000k','mimeTypes'=>
+                    ['application/pdf'],'mimeTypesMessage'=>'Veuillez choisir un document valide'])]])
             ->add('pnomEtud')
-            ->add('dtnsEtud')
             ->add('adEtud')
             ->add('cpEtud')
             ->add('villeEtud')
