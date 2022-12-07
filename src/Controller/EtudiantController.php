efbd696c72486140ac560ad8d84b8ab1a7134d02
sezg0002 <harun.sezgin@etudiant.univ-reims.fr>
2022-12-07 16:29:53 +0100

    Modification fonction update et twig

--- src/Controller/EtudiantController.php
+++ src/Controller/EtudiantController.php
@@ -13,6 +13,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[IsGranted('ROLE_ETUDIANT')]
