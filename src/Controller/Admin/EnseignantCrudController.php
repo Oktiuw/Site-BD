017305e0169b633fbdc05cab74726dfefd1fab48
Aurelien VINCENT <aurelien.vincent@etudiant.univ-reims.fr>
2022-12-08 21:36:09 +0100

    ajout champs enseignant

--- src/Controller/Admin/EnseignantCrudController.php
+++ src/Controller/Admin/EnseignantCrudController.php
@@ -3,7 +3,11 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Enseignant;
+use Doctrine\ORM\EntityRepository;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class EnseignantCrudController extends AbstractCrudController
 {
@@ -12,14 +16,21 @@ class EnseignantCrudController extends AbstractCrudController
         return Enseignant::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+     TextField::new('numEn')->setLabel('Numero'),
+     TextField::new('nomEn')->setLabel('Nom'),
+     TextField::new('pnomEn')->setLabel('Prénom'),
+            DateField::new('dtnsEn')->setLabel('Date de naissance'),
+     TextField::new('adEn')->setLabel("Adresse"),
+     TextField::new('cpEn')->setLabel("Code postal"),
+     TextField::new('villeEn')->setLabel('Ville'),
+     AssociationField::new('cdUtil')->setFormTypeOptions(['choice_label'=>'login','query_builder'=>function (EntityRepository $entityRepository) {
+         return $entityRepository->createQueryBuilder('c')->orderBy('c.login', 'ASC');
+     }])->formatValue(function ($value, $entity) {
+         return $entity->getCdUtil()->getLogin();
+     })->setLabel('login')->hideOnForm(),
+ ];
     }
-    */
 }
