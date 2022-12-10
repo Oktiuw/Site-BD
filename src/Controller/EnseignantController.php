f3223dc62fc41aae43f4e077ef2cd9a8744e1547
sial0003 <khadija.siala@etudiant.univ-reims.fr>
2022-12-10 11:18:23 +0100

    update enseignant

--- src/Controller/EnseignantController.php
+++ src/Controller/EnseignantController.php
@@ -2,10 +2,9 @@
 
 namespace App\Controller;
 
-use App\Form\EntrepriseType;
+use App\Form\EnseignantType;
 use App\Form\UtilisateurType;
 use App\Repository\EnseignantRepository;
-use App\Repository\EntrepriseRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -20,11 +19,45 @@ class EnseignantController extends AbstractController
     public function index(EnseignantRepository $enseignantRepository): Response
     {
         $user=$this->getUser();
-        $profile=$enseignantRepository->findOneBy(['id'=>$user->getId()]);
+        $profile=$enseignantRepository->findOneBy(['cdUtil'=>$user->getId()]);
+        $avatar=null;
+        if ($user->getAvatar() !== null) {
+            $avatar=$user->setAvatar(base64_encode(stream_get_contents($user->getAvatar())));
+        }
         return $this->render('enseignant/index.html.twig', [
-            'user' =>$user,'profile'=>$profile]);
+            'user' =>$user,'profile'=>$profile,'avatar'=>$avatar
+        ]);
+    }
+    #[Route('/enseignant/update')]
+    public function update(EnseignantRepository $enseignantRepository, ManagerRegistry $doctrine, Request $request, UserPasswordHasherInterface $hasher): Response
+    {
+        $enseignantType=new EnseignantType();
+        $user=$this->getUser();
+        $enseignant=$enseignantRepository->findOneBy(['cdUtil'=>$user->getId()]);
+        $formUser=$this->createForm(UtilisateurType::class, $user);
+
+        $form=$this->createForm(EnseignantType::class, $enseignant)->add(
+            'submit',
+            SubmitType::class,
+            ['label' => 'Modifier']
+        );
+        $form->handleRequest($request);
+        $formUser->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $enseignantType=$form->getData();
+            $userData=$formUser->getData();
+            $entityManager=$doctrine->getManager();
+            $enseignant->setNomEn($enseignantType->getNomEn());
+            $enseignant->setPnomEn($enseignantType->getPnomEn());
+            $enseignant->getCdUtil()->setEmail($userData->getEmail());
+            $entityManager->flush();
+            return $this->redirectToRoute('app_enseignant');
+        }
+        return $this->renderForm('enseignant/update', ['form'=>$form,'profile'=>$enseignant,'user'=>$user,'formUser'=>$formUser]);
     }
 }
+
+
 
 
 
