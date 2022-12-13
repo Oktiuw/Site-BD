89c3d22ed8384107250c5f3a064bf645865a029d
Aurelien VINCENT <aurelien.vincent@etudiant.univ-reims.fr>
2022-12-13 15:26:43 +0100

    tjrs probleme avec groupe etudiant

--- src/Controller/Admin/GroupeEtudiantsCrudController.php
+++ src/Controller/Admin/GroupeEtudiantsCrudController.php
@@ -27,28 +27,26 @@ class GroupeEtudiantsCrudController extends AbstractCrudController
     {
         return [
             TextField::new('nomGroupe'),
-            AssociationField::new('etudiants')->formatValue(function ($value, $entity) {
-                $res="";
-                $compteur=0;
-                foreach ($entity->getEtudiants() as $etudiant) {
-                    $res .="{$etudiant->getNomEtud()} {$etudiant->getPnomEtud()}/";
-                    $compteur+=1;
-                    if ($compteur>4) {
-                        $res.=nl2br("\n");
-                        $compteur=0;
-                    }
-                }
-                return $res;
-            })->setFormTypeOptions(['choice_label'=>function (Etudiant $etudiant) {
-                return strtoupper($etudiant->getNomEtud()).' '.$etudiant->getPnomEtud();
-            },'query_builder'=>function (EntityRepository $entityRepository) {
-                return $entityRepository->createQueryBuilder('c')->orderBy('c.nomEtud,c.pnomEtud', 'ASC');
-            }]),
             AssociationField::new('niveau')->formatValue(function ($value, $entity) {
                 return $entity->getNiveau()->getLibNiv();
             })->setFormTypeOptions(['choice_label'=>'LibNiv', 'query_builder'=>function (EntityRepository $entityRepository) {
                 return $entityRepository->createQueryBuilder('c')->orderBy('c.libNiv', 'ASC');
             }]),
+            AssociationField::new('etudiants')
+                ->setFormTypeOptions(['choice_label'=>'nomEtud'])->formatValue(function ($value, $entity) {
+                    $res="";
+                    $compteur=0;
+                    foreach ($entity->getEtudiants() as $etudiant) {
+                        $compteur+=1;
+                        $lastname=strtoupper($etudiant->getNomEtud());
+                        $res .= " {$lastname} {$etudiant->getPnomEtud()}";
+                        if ($compteur>4) {
+                            $compteur=0;
+                            $res.=nl2br("\n");
+                        }
+                    }
+                    return $res;
+                }),
         ];
     }
 }
