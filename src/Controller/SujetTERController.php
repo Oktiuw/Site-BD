907695ff8b237a1b3afbf9b7d847c2e2e0fe7afe
Alix <alix.payraudeau@etudiant.univ-reims.fr>
2022-12-13 16:52:42 +0100

    MAJ du controller, 1 seul route finalement

--- src/Controller/SujetTERController.php
+++ src/Controller/SujetTERController.php
@@ -16,31 +16,18 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 class SujetTERController extends AbstractController
 {
     #[Route('/sujetter', name: 'app_sujet_ter')]
-    #[IsGranted('ROLE_ETUDIANT')]
-    public function indexEtudiant(SujetTERRepository $sujetTERRepository, EtudiantRepository $etudiantRepository): Response
+    public function index(SujetTERRepository $sujetTERRepository, EtudiantRepository $etudiantRepository, EnseignantRepository $enseignantRepository): Response
     {
         $user=$this->getUser();
         $etudiant=$etudiantRepository->findOneBy(['cdUtil'=>$user->getId()]);
-
-        $sujetsTER = $sujetTERRepository->findBy([], ['titreTer'=> 'ASC']);
-        return $this->render('sujet_ter/index.html.twig', [
-            'sujetsTER' => $sujetsTER,
-            'user' => $etudiant
-        ]);
-    }
-
-
-    #[Route('/sujetter', name: 'app_sujet_ter')]
-    #[IsGranted('ROLE_ENSEIGNANT')]
-    public function indexEnseignant(SujetTERRepository $sujetTERRepository, EnseignantRepository $enseignantRepository): Response
-    {
-        $user=$this->getUser();
         $enseignant=$enseignantRepository->findOneBy(['cdUtil'=>$user->getId()]);
 
         $sujetsTER = $sujetTERRepository->findBy([], ['titreTer'=> 'ASC']);
         return $this->render('sujet_ter/index.html.twig', [
             'sujetsTER' => $sujetsTER,
-            'user' => $enseignant
+            'etudiant' => $etudiant,
+            'enseignant' => $enseignant
         ]);
     }
+
 }
