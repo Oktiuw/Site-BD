cb31478fca3eac18c606fe66c373e0def65224cd
Aurélien Vincent <aurelien.vincent@etudiants.univ-reims.fr>
2022-12-14 11:56:56 +0100

    ajout redirection si compte désactivé

--- src/Controller/RedirecteurController.php
+++ src/Controller/RedirecteurController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\EntrepriseRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,12 +10,17 @@ use Symfony\Component\Routing\Annotation\Route;
 class RedirecteurController extends AbstractController
 {
     #[Route('/redirecteur', name: 'app_redirecteur')]
-    public function index(): Response
+    public function index(EntrepriseRepository $entrepriseRepository): Response
     {
         if ($this->getUser()->getRoles()[0]=="ROLE_ETUDIANT") {
             return $this->redirectToRoute('app_etudiant');
         }
         if ($this->getUser()->getRoles()[0]=="ROLE_ENTREPRISE") {
+            $entreprise=$entrepriseRepository->find($this->getUser()->getId());
+            if ($entreprise->isIsDisabled())
+            {
+                return $this->render('entreprise/succes.html.twig');
+            }
             return $this->redirectToRoute('app_entreprise');
         }
         return $this->redirectToRoute('app_enseignant');
