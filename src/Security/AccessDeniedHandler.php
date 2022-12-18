75c0e88d934d08215174ea2659afb4aac027e180
Aurelien VINCENT <aurelien.vincent@etudiant.univ-reims.fr>
2022-12-18 10:40:00 +0100

    Gestion Erreur OK

--- src/Security/AccessDeniedHandler.php
+++ src/Security/AccessDeniedHandler.php
@@ -6,11 +6,11 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
-class AccesDeniedHandler implements \Symfony\Component\Security\Http\Authorization\AccessDeniedHandlerInterface
+class AccessDeniedHandler implements \Symfony\Component\Security\Http\Authorization\AccessDeniedHandlerInterface
 {
     public function handle(Request $request, AccessDeniedException $accessDeniedException): Response
     {
-        $content="Vous n'avez pas le droit d'accèder à cette ressource";
+        $content="<h1>Vous n'avez pas le droit d'accèder à cette ressource</h1> <a href='/'>Accueil</a>";
         return new Response($content, 403);
     }
 }
