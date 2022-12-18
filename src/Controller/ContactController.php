a45036229d6542ae460490b93d1ff647bb1afe0c
Aurelien VINCENT <aurelien.vincent@etudiant.univ-reims.fr>
2022-12-18 14:27:58 +0100

    Ajout vérif si compte pas encore actif

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,18 +2,27 @@
 
 namespace App\Controller;
 
+use App\Repository\EntrepriseRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
 #[IsGranted('IS_AUTHENTICATED_FULLY')]
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact')]
-    public function index(): Response
+    public function index(EntrepriseRepository $entrepriseRepository): Response
     {
+        $user=$this->getUser();
+        if ($user->getRoles()[0]==='ROLE_ENTREPRISE') {
+            $entreprise=$entrepriseRepository->findOneBy(['cdUtil'=>$user->getId()]);
+            if ($entreprise->isIsdisabled()) {
+                return $this->redirectToRoute('app_redirecteur');
+            }
+        }
         return $this->render('contact/index.html.twig', [
-            'controller_name' => 'ContactController',
+            'user' => $user,
         ]);
     }
 }
