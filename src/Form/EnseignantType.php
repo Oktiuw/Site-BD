6749c686bd88ed69945dc706b3a4c702186cb557
sial0003 <khadija.siala@etudiant.univ-reims.fr>
2022-12-22 00:56:01 +0100

    Modification du formulaire enseignant

--- src/Form/EnseignantType.php
+++ src/Form/EnseignantType.php
@@ -7,14 +7,17 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-
 class EnseignantType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nomEn')
-            ->add('pnomEn');
+
+            ->add('adEn')
+            ->add('cpEn')
+            ->add('villeEn')
+
+        ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
