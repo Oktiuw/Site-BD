d2efe90fc58da950735c3f1df3347033079284f5
Aurelien VINCENT <aurelien.vincent@etudiant.univ-reims.fr>
2022-12-21 13:30:06 +0100

    ajout getTypeUser

--- src/Controller/EmploiDuTempsController.php
+++ src/Controller/EmploiDuTempsController.php
@@ -2,10 +2,14 @@
 
 namespace App\Controller;
 
+use App\Repository\EnseignantRepository;
+use App\Repository\EtudiantRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Security("is_granted('ROLE_ETUDIANT') or is_granted('ROLE_ENSEIGNANT')")]
 class EmploiDuTempsController extends AbstractController
 {
     #[Route('/emploi/du/temps', name: 'app_emploi_du_temps')]
@@ -14,5 +18,13 @@ class EmploiDuTempsController extends AbstractController
         return $this->render('emploi_du_temps/index.html.twig', [
             'controller_name' => 'EmploiDuTempsController',
         ]);
+    }
+    public function getTypeUser(EtudiantRepository $etudiantRepository, EnseignantRepository $enseignantRepository): \App\Entity\Etudiant|\App\Entity\Enseignant|null
+    {
+        $user=$this->getUser();
+        if ($user->getRoles()[0]=="ROLE_ETUDIANT") {
+            return $etudiantRepository->findOneBy(['cdUtil'=>$user->getId()]);
+        }
+        return $enseignantRepository->findOneBy(['cdUtil'=>$user->getId()]);
     }
 }
