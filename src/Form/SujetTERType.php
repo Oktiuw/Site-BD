b4ad364f4b8ea4d68f84eda37f0532fe478d909d
Alix <alix.payraudeau@etudiant.univ-reims.fr>
2022-12-21 16:37:59 +0100

    creation du formulaire en lien avec l'entite SujetTER

--- src/Form/SujetTERType.php
+++ src/Form/SujetTERType.php
@@ -2,10 +2,14 @@
 
 namespace App\Form;
 
+use App\Entity\Enseignant;
+use App\Entity\Niveau;
 use App\Entity\SujetTER;
+use Doctrine\ORM\EntityRepository;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 class SujetTERType extends AbstractType
 {
@@ -14,9 +18,16 @@ class SujetTERType extends AbstractType
         $builder
             ->add('titreTer')
             ->add('descTer')
-            ->add('niveau')
-            ->add('Enseignant')
-            ->add('Etudiant')
+            ->add('niveau', EntityType::class, [
+                            'required' => true,
+                            'placeholder' => 'Niveau ?',
+                            'class' => Niveau::class,
+                            'choice_label' => 'libNiv',
+                            'query_builder' => function (EntityRepository $entityRepository) {
+                                return $entityRepository->createQueryBuilder('niv')
+                                    ->orderBy('niv.libNiv', 'ASC');
+                            },
+                ])
         ;
     }
 
