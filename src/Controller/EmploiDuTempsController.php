5214f2f53880803183ec939b154fb754cafe0980
Aurelien VINCENT <aurelien.vincent@etudiant.univ-reims.fr>
2022-12-21 20:40:36 +0100

    ajout vérif bon pattern

--- src/Controller/EmploiDuTempsController.php
+++ src/Controller/EmploiDuTempsController.php
@@ -5,20 +5,27 @@ namespace App\Controller;
 use App\Entity\Enseignant;
 use App\Repository\EnseignantRepository;
 use App\Repository\EtudiantRepository;
+use phpDocumentor\Reflection\Types\Boolean;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Security("is_granted('ROLE_ETUDIANT') or is_granted('ROLE_ENSEIGNANT')")]
+#[Security("is_granted('ROLE_ETUDIANT') or is_granted('ROLE_ENSEIGNANT') or is_granted('ROLE_ADMIN')")]
 class EmploiDuTempsController extends AbstractController
 {
     #[Route('/emploidutemps', name: 'app_emploi_du_temps')]
-    public function index(EtudiantRepository $etudiantRepository, EnseignantRepository $enseignantRepository, $currentDate=null): Response
+    public function index(): \Symfony\Component\HttpFoundation\RedirectResponse
     {
-        if ($currentDate===null) {
-            $currentDate= new \DateTime();
-            $currentDate->setTimezone(new \DateTimeZone('Europe/Paris'));
+        $date=new \DateTime();
+        return $this->redirectToRoute("app_emploi_du_temps_date", ['currentDate'=>$date->format('d-m-y')]);
+    }
+    #[Route('/emploidutemps/{currentDate}', name: 'app_emploi_du_temps_date')]
+    public function edtdate(EtudiantRepository $etudiantRepository, EnseignantRepository $enseignantRepository, $currentDate): Response
+    {
+        $currentDate=\DateTime::createFromFormat('d-m-y', $currentDate);
+        if ($currentDate===false) {
+            return $this->redirectToRoute('app_emploi_du_temps');
         }
         $user=$this->getTypeUser($etudiantRepository, $enseignantRepository);
         if ($user instanceof (Enseignant::class)) {
@@ -43,7 +50,7 @@ class EmploiDuTempsController extends AbstractController
             return ($a->getHDeb()<$b->getHDeb() ? -1 : 1);
         });
         return $this->render('emploi_du_temps/index.html.twig', [
-            'date' =>$currentDate->format('d/m/Y'),'cours'=>$cours,'user'=>$this->getUser()
+            'date' =>$currentDate,'cours'=>$cours,'user'=>$this->getUser(),
         ]);
     }
     public function getTypeUser(EtudiantRepository $etudiantRepository, EnseignantRepository $enseignantRepository): \App\Entity\Etudiant|\App\Entity\Enseignant|null
