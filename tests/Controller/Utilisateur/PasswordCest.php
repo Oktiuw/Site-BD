890fe253128641db0840c4f5776ff99e482def83
sezg0002 <harun.sezgin@etudiant.univ-reims.fr>
2022-12-23 17:54:00 +0100

    Test modif mot de passe

---
+++ tests/Controller/Utilisateur/PasswordCest.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Tests\Controller\Utilisateur;
+
+use App\Factory\UtilisateurFactory;
+use App\Tests\Support\ControllerTester;
+
+class PasswordCest
+{
+    public function password(ControllerTester $I): void
+    {
+        $user = UtilisateurFactory::createOne(['roles' => ['IS_AUTHENTICATED_FULLY']]);
+        $I->amLoggedInAs($user->object());
+        $I->amOnPage('/updatePassword');
+        $I->see("Modification de votre mot de passe", 'h1');
+    }
+}
