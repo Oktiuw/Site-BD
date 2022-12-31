872eeeb2b2b5610ea34af75971c228bad077c2fa
Fouad Belhia <fouad.belhia@etudiant.univ-reims.fr>
2022-12-31 10:48:41 +0100

    Ajout de la route pour créer un stage dans le controller

--- src/Controller/StageController.php
+++ src/Controller/StageController.php
@@ -2,12 +2,17 @@
 
 namespace App\Controller;
 
+use App\Entity\Stage;
+use App\Form\StageType;
 use App\Repository\EnseignantRepository;
 use App\Repository\EntrepriseRepository;
 use App\Repository\EtudiantRepository;
 use App\Repository\StageRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -29,6 +34,29 @@ class StageController extends AbstractController
             'user' => $user,
             'etudiant' => $etudiant,
             'entreprise' => $entreprise
+        ]);
+    }
+
+    #[Route('/stage/create')]
+    #[IsGranted('ROLE_ENTREPRISE')]
+    public function create(ManagerRegistry $doctrine, Request $request, EntrepriseRepository $entrepriseRepository)
+    {
+        $stage = new Stage();
+        $stage->setEntreprise($entrepriseRepository->findOneBy(['cdUtil'=>$this->getUser()->getId()]));
+
+        $form = $this->createForm(StageType::class, $stage);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $doctrine->getManager()->persist($stage);
+            $doctrine->getManager()->flush();
+            return $this->redirectToRoute('app_stage');
+        }
+
+        return $this->renderForm('stage/create.html.twig', [
+            'stage' => $stage,
+            'form' => $form,
         ]);
     }
 }
