df1ec581dc7054cc7c8a55a0fefc763fe5244670
Fouad Belhia <fouad.belhia@etudiant.univ-reims.fr>
2022-12-31 10:47:34 +0100

    Mise en place du formulaire de création d'un stage

---
+++ src/Form/StageType.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Niveau;
+use App\Entity\Stage;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+
+class StageType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('titreStage', TextType::class, ['attr' => ['oninvalid' => "setCustomValidity('Titre manquant')"]])
+            ->add('descStage', TextareaType::class, ['attr' => ['style' => 'height:250px',
+                                                                    'oninvalid' => "setCustomValidity('Descriptif manquant')"]])
+            ->add('niveau', EntityType::class, [
+                            'required' => true,
+                            'attr' => ['oninvalid' => "setCustomValidity('Veuillez sélectionner un niveau')"],
+                            'placeholder' => 'Niveau ?',
+                            'class' => Niveau::class,
+                            'choice_label' => 'libNiv',
+                            'query_builder' => function (EntityRepository $entityRepository) {
+                                return $entityRepository->createQueryBuilder('niv')
+                                    ->orderBy('niv.libNiv', 'ASC');
+                            },
+                ])
+            ->add('adStage', TextType::class, ['attr' => ['oninvalid' => "setCustomValidity('Adresse manquante')"]])
+            ->add('cpStage', TextType::class, ['attr' => ['oninvalid' => "setCustomValidity('Code postal manquant')"]])
+            ->add('villeStage', TextType::class, ['attr' => ['oninvalid' => "setCustomValidity('Ville manquante')"]])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Stage::class,
+        ]);
+    }
+}
