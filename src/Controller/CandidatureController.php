941f7b0f5be04ae16bbb6d98e282510c83ea5b4e
Fouad Belhia <fouad.belhia@etudiant.univ-reims.fr>
2023-01-01 17:02:46 +0100

    Ajout d'une vérification sur l'url: si stage n'appartient pas à l'entreprise connectée, redirection

--- src/Controller/CandidatureController.php
+++ src/Controller/CandidatureController.php
@@ -6,6 +6,7 @@ use App\Entity\Canditatures;
 use App\Repository\EtudiantRepository;
 use App\Repository\StageRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -19,29 +20,28 @@ class CandidatureController extends AbstractController
         $candidature = new Canditatures();
         $candidature->setEtudiant($etudiantRepository->findOneBy(['cdUtil'=>$this->getUser()->getId()]));
         $candidature->setStage($stageRepository->findOneBy(['id'=>$request->get('id')]));
-        $candidature->setPj(file_get_contents('img/cvsae.pdf', true));
-        #$etudiantRepository->findOneBy(['cdUtil'=>$this->getUser()->getId()])->getCvEtud());
 
         $doctrine->getManager()->persist($candidature);
         $doctrine->getManager()->flush();
         return $this->redirectToRoute('app_stage');
     }
 
-    #on veut voir les candidatures d'un stage
     #[Route('/candidature/{id}', name: 'app_candidature')]
-    public function index($id, StageRepository $stageRepository, EtudiantRepository $etudiantRepository): Response
+    #[IsGranted('ROLE_ENTREPRISE')]
+    public function index($id, StageRepository $stageRepository): Response
     {
         $stage = $stageRepository->findOneBy(['id'=>$id]);
         $candidatures = $stage->getCanditatures();
         $user = $this->getUser();
-        
+
+        if ($stage->getEntreprise()->getCdUtil()->getId() != $user->getId()) {
+            return $this->redirectToRoute('app_stage');
+        }
+
         return $this->render('candidature/index.html.twig', [
             'canditatures' => $candidatures,
             'stage' => $stage,
             'user' => $user,
         ]);
     }
-
-
-    
 }
