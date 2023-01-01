dda25b1d16509a2f18a8b687b249014a7ba3f944
Alix <alix.payraudeau@etudiant.univ-reims.fr>
2023-01-01 18:18:00 +0100

    Ajout d'un test pour l'acces refusé des entreprises + meilleure gestion des noms de fonctions de tests

--- tests/Controller/SujetTER/IndexSujetTERCest.php
+++ tests/Controller/SujetTER/IndexSujetTERCest.php
@@ -5,11 +5,12 @@ namespace App\Tests\Controller\SujetTER;
 
 use App\Tests\Support\ControllerTester;
 use App\Factory\UtilisateurFactory;
+use Codeception\Util\HttpCode;
 
 
 class IndexSujetTERCest
 {
-    public function OnPageSujetTerForEtudiant(ControllerTester $I)
+    public function AccessForEtudiant(ControllerTester $I)
     {
         $user = UtilisateurFactory::createOne(['roles' => ['ROLE_ETUDIANT']]);
         $I->amLoggedInAs($user->object());
@@ -18,7 +19,7 @@ class IndexSujetTERCest
         $I->seeInTitle('Liste des Sujets TER');
         $I->see('Liste des Sujets TER', 'h1');
     }
-    public function OnPageSujetTerForEnseignant(ControllerTester $I)
+    public function AccessForEnseignant(ControllerTester $I)
     {
         $user = UtilisateurFactory::createOne(['roles' => ['ROLE_ENSEIGNANT']]);
         $I->amLoggedInAs($user->object());
@@ -26,5 +27,13 @@ class IndexSujetTERCest
         $I->seeResponseCodeIsSuccessful();
         $I->seeInTitle('Liste des Sujets TER');
         $I->see('Liste des Sujets TER', 'h1');
+    }
+
+    public function NoAccessForEntreprise(ControllerTester $I)
+    {
+        $user = UtilisateurFactory::createOne(['roles' => ['ROLE_ENTREPRISE']]);
+        $I->amLoggedInAs($user->object());
+        $I->amOnPage('/sujetter');
+        $I->seeResponseCodeIs(HttpCode::FORBIDDEN);
     }
 }
