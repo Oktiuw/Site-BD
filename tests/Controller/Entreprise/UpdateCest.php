4bb2b7e5b415b95298b1c01fc5be45567b272e66
sezg0002 <harun.sezgin@etudiant.univ-reims.fr>
2023-01-03 21:15:51 +0100

    Correction roles dans test update Entreprise petite fausse manip

--- tests/Controller/Entreprise/UpdateCest.php
+++ tests/Controller/Entreprise/UpdateCest.php
@@ -11,21 +11,16 @@ class UpdateCest
 {
     public function form(ControllerTester $I)
     {
-        $user = UtilisateurFactory::createOne(['roles' => ['ROLE_ENSEIGNANT']]);
-
+        $user = UtilisateurFactory::createOne(['roles' => ['ROLE_ENTREPRISE']]);
         EntrepriseFactory::createOne([
             'nomRef' => 'Jean Jean',
             'nomEnt' => 'SARL Jean',
             'cdUtil' => $user,
             'telEnt' => null,
             'isDisabled' => false,
-
         ]);
-
         $I->amLoggedInAs($user->object());
         $I->amOnPage('/entreprise/1/update');
 
     }
-
-
 }
