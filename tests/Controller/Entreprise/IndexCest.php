7ae3726f02622b2a8312763fc230583a9f0250f8
sezg0002 <harun.sezgin@etudiant.univ-reims.fr>
2023-01-03 21:53:12 +0100

    Test contact d'un groupe etudiant depuis un compte entreprise

--- tests/Controller/Entreprise/IndexCest.php
+++ tests/Controller/Entreprise/IndexCest.php
@@ -32,6 +32,24 @@ class IndexCest
         $I->see('Envoi de mail', 'h1');
     }
 
+    public function contactGroupe(ControllerTester $I)
+    {
+        $user = UtilisateurFactory::createOne(['roles' => ['ROLE_ENTREPRISE']]);
+        EntrepriseFactory::createOne([
+            'nomRef' => 'Jean Jean',
+            'nomEnt' => 'SARL Jean',
+            'cdUtil' => $user,
+            'telEnt' => null,
+            'isDisabled' => false,
+        ]);
+        $I->amLoggedInAs($user->object());
+        $I->amOnPage('/contact/groupeEtudiants');
+        $I->seeInTitle("Contact");
+        $I->see('Envoi de mail', 'h1');
+    }
+
+
+
 
 
 }
