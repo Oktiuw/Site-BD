e47eaf539335553f83ca067b28d1d0b4d757f54c
Aurélien Vincent <aurelien.vincent@etudiants.univ-reims.fr>
2023-01-03 16:09:32 +0100

    Nouvelles fixtures OK

--- src/DataFixtures/EtudiantFixtures.php
+++ src/DataFixtures/EtudiantFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Niveau;
 use App\Entity\Utilisateur;
 use App\Factory\EtudiantFactory;
 use App\Factory\UtilisateurFactory;
@@ -13,11 +14,12 @@ class EtudiantFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
-        EtudiantFactory::createMany(50, function () {
+        $niveaux=$manager->getRepository(Niveau::class)->findAll();
+        EtudiantFactory::createMany(50, function () use ($niveaux) {
             $lastname=EtudiantFactory::faker()->unique()->lastName();
-            return ['nomEtud'=>"$lastname",'cdUtil'=>UtilisateurFactory::createOne(["roles"=>["ROLE_ETUDIANT"],"login"=>strtolower(str_replace(" ", "", "$lastname".rand(1, 300)))])];
+            return ['niveau'=>$niveaux[array_rand($niveaux)],'nomEtud'=>"$lastname",'cdUtil'=>UtilisateurFactory::createOne(["roles"=>["ROLE_ETUDIANT"],"login"=>strtolower(str_replace(" ", "", "$lastname".rand(1, 300)))])];
         });
-        EtudiantFactory::createOne(['cdUtil'=>UtilisateurFactory::createOne(["login"=>"Etudiant","roles"=>["ROLE_ETUDIANT"]])]);
+        EtudiantFactory::createOne(['niveau'=>$niveaux[0],'cdUtil'=>UtilisateurFactory::createOne(["login"=>"Etudiant","roles"=>["ROLE_ETUDIANT"]])]);
     }
 
     public function getDependencies()
