dd10ee5d2462818b1124a47862d5bccdbcefcba3
Alix <alix.payraudeau@etudiant.univ-reims.fr>
2023-01-04 08:57:20 +0100

    adaptation des acces pour les administrateurs (role 'ROLE_ADMIN,ROLE_ENSEIGNANT')

--- src/Controller/SujetTERController.php
+++ src/Controller/SujetTERController.php
@@ -63,7 +63,7 @@ class SujetTERController extends AbstractController
     public function update(ManagerRegistry $doctrine, SujetTER $sujetTER, Request $request, EnseignantRepository $enseignantRepository)
     {
         #un Enseignant ne peut modifier qu'un sujet TER dont il est le responsable
-        if ($enseignantRepository->findOneBy(['cdUtil'=>$this->getUser()->getId()]) === $sujetTER->getEnseignant()) {
+        if ($enseignantRepository->findOneBy(['cdUtil'=>$this->getUser()->getId()]) === $sujetTER->getEnseignant() or $this->getUser()->getRoles()[0]==='ROLE_ADMIN,ROLE_ENSEIGNANT') {
             $form = $this->createForm(SujetTERType::class, $sujetTER);
 
             $form->handleRequest($request);
@@ -86,7 +86,7 @@ class SujetTERController extends AbstractController
     public function delete(ManagerRegistry $doctrine, SujetTER $sujetTER, EnseignantRepository $enseignantRepository)
     {
         #un Enseignant ne peut supprimer qu'un sujet TER dont il est le responsable
-        if ($enseignantRepository->findOneBy(['cdUtil'=>$this->getUser()->getId()]) === $sujetTER->getEnseignant()) {
+        if ($enseignantRepository->findOneBy(['cdUtil'=>$this->getUser()->getId()]) === $sujetTER->getEnseignant() or $this->getUser()->getRoles()[0]==='ROLE_ADMIN,ROLE_ENSEIGNANT') {
             $doctrine->getManager()->remove($sujetTER);
             $doctrine->getManager()->flush();
         }
