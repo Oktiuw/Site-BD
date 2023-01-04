50de3cf3cbc1fa0ccb3ca2648b5bd4074836abea
Aurelien VINCENT <aurelien.vincent@etudiant.univ-reims.fr>
2023-01-04 10:41:42 +0100

    rendre impossible la suppression ou modification de niveau

--- src/Controller/Admin/NiveauCrudController.php
+++ src/Controller/Admin/NiveauCrudController.php
@@ -3,6 +3,8 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Niveau;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
@@ -18,5 +20,10 @@ class NiveauCrudController extends AbstractCrudController
         return [
             TextField::new('LibNiv')->setLabel("Nom niveau"),
         ];
+    }
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions
+            ->disable(Action::DELETE,Action::EDIT);
     }
 }
