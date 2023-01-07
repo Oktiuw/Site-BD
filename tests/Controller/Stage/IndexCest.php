89f89963f53350588dd1936e5293c9b8cbb103eb
Aurelien VINCENT <aurelien.vincent@etudiant.univ-reims.fr>
2023-01-07 16:27:12 +0100

    del et update Stage OK

--- tests/Controller/Stage/IndexCest.php
+++ tests/Controller/Stage/IndexCest.php
@@ -3,6 +3,8 @@
 namespace App\Tests\Controller\Stage;
 
 use App\Factory\EntrepriseFactory;
+use App\Factory\NiveauFactory;
+use App\Factory\StageFactory;
 use App\Factory\UtilisateurFactory;
 use App\Tests\Support\ControllerTester;
 
@@ -20,5 +22,23 @@ class IndexCest
         $I->amLoggedInAs($user->object());
         $I->amOnPage('/stage');
         $I->see('Ajouter un nouveau stage', 'a.btn-dark');
+    }
+    public function updateStage(ControllerTester $I)
+    {
+        $user = UtilisateurFactory::createOne(['roles' => ['ROLE_ENTREPRISE']]);
+        $entreprise=EntrepriseFactory::createOne(['cdUtil'=>$user]);
+        $stage=StageFactory::createOne(['entreprise'=>$entreprise,'niveau'=>NiveauFactory::createOne(['libNiv'=>'M1'])]);
+        $I->amLoggedInAs($user->object());
+        $I->amOnPage('/stage');
+        $I->see('Modifier', 'a');
+    }
+    public function delStage(ControllerTester $I)
+    {
+        $user = UtilisateurFactory::createOne(['roles' => ['ROLE_ENTREPRISE']]);
+        $entreprise=EntrepriseFactory::createOne(['cdUtil'=>$user]);
+        $stage=StageFactory::createOne(['entreprise'=>$entreprise,'niveau'=>NiveauFactory::createOne(['libNiv'=>'M1'])]);
+        $I->amLoggedInAs($user->object());
+        $I->amOnPage('/stage');
+        $I->see('Supprimer', 'a');
     }
 }
