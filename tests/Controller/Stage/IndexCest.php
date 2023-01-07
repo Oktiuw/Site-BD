6da2126edd10b6b042871422ff404f71020e8bfe
Aurelien VINCENT <aurelien.vincent@etudiant.univ-reims.fr>
2023-01-07 16:21:43 +0100

    addStage OK

--- tests/Controller/Stage/IndexCest.php
+++ tests/Controller/Stage/IndexCest.php
@@ -1,8 +1,9 @@
 <?php
 
-
 namespace App\Tests\Controller\Stage;
 
+use App\Factory\EntrepriseFactory;
+use App\Factory\UtilisateurFactory;
 use App\Tests\Support\ControllerTester;
 
 class IndexCest
@@ -10,6 +11,14 @@ class IndexCest
     public function restriction(ControllerTester $I)
     {
         $I->amOnPage('/stage');
-        $I->see('Identifiez-vous','h1');
+        $I->see('Identifiez-vous', 'h1');
+    }
+    public function addStage(ControllerTester $I)
+    {
+        $user = UtilisateurFactory::createOne(['roles' => ['ROLE_ENTREPRISE']]);
+        $entreprise=EntrepriseFactory::createOne(['cdUtil'=>$user]);
+        $I->amLoggedInAs($user->object());
+        $I->amOnPage('/stage');
+        $I->see('Ajouter un nouveau stage', 'a.btn-dark');
     }
 }
